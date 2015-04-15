8cca34768ab60605944fb099350ea08366aa9158
Andy Bavier <acb@cs.princeton.edu>
2015-04-15 16:55:38 -0400

    Bug fixes

--- scripts/network-setup.sh
+++ scripts/network-setup.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-source ~/admin_openrc.sh
+source ~/admin-openrc.sh
 
 # Create nat-net network
-neutron net-show nat-net 2&>1 > /dev/null
+neutron net-show nat-net 2>&1 > /dev/null
 if [ "$?" -ne 0 ]
 then
     neutron net-create --provider:physical_network=nat --provider:network_type=flat --shared nat-net
 fi
 
 # Create nat-net subnet
-neutron subnet-show nat-net 2&>1 > /dev/null
+neutron subnet-show nat-net 2>&1 > /dev/null
 if [ "$?" -ne 0 ]
 then
     neutron subnet-create nat-net --name nat-net 172.16.0.0/16 --gateway=172.16.0.1 --enable-dhcp=false
 fi
 
 # Create nat-net network
-neutron net-show ext-net 2&>1 > /dev/null
+neutron net-show ext-net 2>&1 > /dev/null
 if [ "$?" -ne 0 ]
 then
     neutron net-create --provider:physical_network=ext --provider:network_type=flat --shared ext-net
