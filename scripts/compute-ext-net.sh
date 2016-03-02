418b982e7a9d2fd4be9fac7137b59c51ddc04e12
Andy Bavier <acb@cs.princeton.edu>
2016-03-02 14:38:38 -0500

    Fix addresses to avoid conflict with LAN net on CORD

--- scripts/compute-ext-net.sh
+++ scripts/compute-ext-net.sh
@@ -3,9 +3,9 @@
 apt-get update
 apt-get install bridge-utils
 brctl addbr databr
-ifconfig databr 192.168.0.254/24 up
+ifconfig databr 10.168.0.1/24 up
 ip link add address 02:42:0a:a8:00:01 type veth
 ifconfig veth0 up
 ifconfig veth1 up
 brctl addif databr veth0
-iptables -t nat -A POSTROUTING -s 192.168.0.0/24 \! -d 192.168.0.0/24 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.168.0.0/24 \! -d 10.168.0.0/24 -j MASQUERADE
